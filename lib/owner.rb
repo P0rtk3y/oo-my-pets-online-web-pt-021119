45942d719ea4c73e760d1247829eb23075fb049a
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-22 06:30:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,7 +52,7 @@ class Owner
   end
   
   def list_pets 
-    puts "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats}.count} cat."
+    puts "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats}.count} cat(s)."
   end 
   
   def self.all 
